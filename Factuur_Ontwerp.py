6faa2243b8793436fce5fcc650d8cb56d122ee50
Vuzaaa <99075586@mydavinci.nl>
2024-04-15 14:12:01 +0200

    Einde eerste dag design op de helft

---
+++ Factuur_Ontwerp.py
@@ -0,0 +1,54 @@
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.units import inch
+
+# Times-Italic voor de factuur titel
+# Helvetica-Bold voor de broski titel 
+# Helvetica voor de tekst
+
+pdffile = Canvas('Factuur Broski.pdf')
+
+fonts = pdffile.getAvailableFonts()
+
+pdffile.setFont('Times-Italic', 35)
+pdffile.drawString(240, 750, 'Factuur')
+
+pdffile.setFont('Helvetica-Bold', 25)
+pdffile.drawString(205, 715, ' B  R  O  S  K  I ')
+
+pdffile.setFont('Helvetica-Bold', 13)
+pdffile.drawString(80, 680, 'Factuur Broski')
+
+pdffile.setFont('Helvetica', 13 )
+pdffile.drawString(80, 665, 'F Fitskie & Y. Bron')
+pdffile.drawString(80, 650, 'Rooseveltstraat 9')
+pdffile.drawString(80, 635, '3772 BK Barneveld')
+pdffile.drawString(80, 620, 'Nederland')
+
+pdffile.drawString(80, 590, 'BTW nummer: NL-843219765B45')
+pdffile.drawString(80, 575, 'KVK nummer: 874302619481 ')
+
+pdffile.drawString(80, 545, 'Factuurnummer: F2024-01') # Nog dynamisch maken
+pdffile.drawString(80, 530, '15-04-24') # Nog dynamisch maken
+
+pdffile.setFont('Helvetica-Bold', 13)
+pdffile.drawString(380, 680, 'Gegevens klant')
+
+pdffile.setFont('Helvetica', 13 )
+pdffile.drawString(380, 665, 'Naam klant')
+pdffile.drawString(380, 650, 'Adres klant')
+pdffile.drawString(380, 635, 'Postcode en straat klant' )
+pdffile.drawString(380, 620, 'Land klant')
+
+y = 700
+x = 80
+pdffile.line(x, y, x + 400, y)
+
+y = 510
+x = 80
+pdffile.line(x, y, x + 400, y)
+
+
+# Voor de tabel een for loop gebruiken met max aantal regels.
+
+
+pdffile.save()
