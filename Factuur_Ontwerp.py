948f64f2dc0a15b3abdd0bad1984a024c0ab0205
Vuzaaa <99075586@mydavinci.nl>
2024-04-17 09:09:45 +0200

    Design af

--- Factuur_Ontwerp.py
+++ Factuur_Ontwerp.py
@@ -22,6 +22,7 @@ pdffile.drawString(205, 715, ' B  R  O  S  K  I ')
 
 pdffile.setFont('Helvetica-Bold', 13)
 pdffile.drawString(80, 680, 'Factuur Broski')
+pdffile.drawString(380, 680, 'Gegevens klant')
 
 pdffile.setFont('Helvetica', 13 )
 pdffile.drawString(80, 665, 'F Fitskie & Y. Bron')
@@ -34,6 +35,7 @@ pdffile.drawString(80, 575, 'KVK nummer: 874302619481 ')
 
 pdffile.drawString(80, 545, 'Factuurnummer: F2024-01') # Nog dynamisch maken
 pdffile.drawString(80, 530, '15-04-24') # Nog dynamisch maken
+
 
 pdffile.setFont('Helvetica-Bold', 13)
 pdffile.drawString(380, 680, 'Gegevens klant')
@@ -52,8 +54,52 @@ y = 510
 x = 80
 pdffile.line(x, y, x + 400, y)
 
+pdffile.setFont('Helvetica-Bold', 15)
+pdffile.drawString(100, 485, 'Aantal')
+pdffile.drawString(200, 485, 'Item')
+pdffile.drawString(300, 485, 'Prijs')
+pdffile.drawString(400, 485, 'Totaal')
+
+y = 465
+x = 80
+pdffile.line(x, y, x + 400, y)
 
 # Voor de tabel een for loop gebruiken met max aantal regels.
+pdffile.setFont('Helvetica', 13 )
+y = 440
+for i in range(6):
+    pdffile.drawString(103, y, 'Aantal')
+    pdffile.drawString(203, y, 'Item')
+    pdffile.drawString(303, y, 'Prijs')
+    pdffile.drawString(403, y, 'Totaal')
+    y -= 25
+
+y = y  
+x = 80
+pdffile.line(x, y, x + 400, y)
+
+y = y - 45
+x = 80
+pdffile.line(x, y, x + 400, y)
+
+y = y - 20
+pdffile.drawString(340, y, 'Totaal excl BTW: ' )
+pdffile.drawString(450, y, 'Prijs' )
+
+y = y - 20
+pdffile.drawString(376, y, '21% BTW: ' )
+pdffile.drawString(450, y, 'Prijs' )
+
+y = y - 20
+pdffile.setFont('Helvetica-Bold', 13)
+pdffile.drawString(335, y, 'Totaal incl BTW: ' )
+pdffile.drawString(450, y, 'Prijs' )
+
+
+
+
+
+
 
 
 pdffile.save()
